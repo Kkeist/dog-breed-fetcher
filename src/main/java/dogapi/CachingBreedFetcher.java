c6de5ac7221751f930acc0823d8c2a8d190d4ff2
Kkeist <93057353+Kkeist@users.noreply.github.com>
2025-10-11 17:54:30 -0400

    finish task 3+4

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -22,7 +22,7 @@ public class CachingBreedFetcher implements BreedFetcher {
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws  BreedNotFoundException{
         if(cahceMap.containsKey(breed)){
             return cahceMap.get(breed);
         }else{
